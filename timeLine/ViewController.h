75450bcc9584475485156523b765d9355892550e
showhilllee <lxh_302@163.com>
2014-09-22 15:42:33 +0800

    add description

--- timeLine/ViewController.h
+++ timeLine/ViewController.h
@@ -6,6 +6,10 @@
 //  Copyright (c) 2014年 WZC. All rights reserved.
 //
 
+/*
+ * 源码由王*超提供（为保护个人隐私，中间部分文字已用*代替） ~O(∩_∩)O~
+ */
+
 #import <UIKit/UIKit.h>
 
 @interface ViewController : UIViewController{
